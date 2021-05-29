bdf8019238f8b9e6d24156790761ffc8222f2f65
martinsivanov <82410157+martinsivanov@users.noreply.github.com>
2021-05-29 14:13:09 +0300

    Introduced controllers

---
+++ SUS/Apps/MyFirstMvcApp/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using SUS.HTTP;
+using System.Linq;
+using System.Text;
+
+namespace MyFirstMvcApp.Controllers
+{
+    public class HomeController
+    {
+        public HttpResponse Index(HttpRequest request)
+        {
+            var responseHtml = "<h1>Welcome!</h1>" +
+                request.Headers.FirstOrDefault(x => x.Name == "User-Agent")?.Value;
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+        public HttpResponse About(HttpRequest request)
+        {
+            var responseHtml = "<h1>About...</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+            var response = new HttpResponse("text/html", responseBodyBytes);
+            return response;
+        }
+    }
+}

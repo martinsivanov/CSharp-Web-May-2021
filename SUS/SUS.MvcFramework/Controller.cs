366fb93c257112f5e1c951a4c52c2264f299b467
martinsivanov <82410157+martinsivanov@users.noreply.github.com>
2021-06-02 21:39:35 +0300

    Work done for SusViewEngine

--- SUS/SUS.MvcFramework/Controller.cs
+++ SUS/SUS.MvcFramework/Controller.cs
@@ -1,4 +1,5 @@
 ﻿using SUS.HTTP;
+using SUS.MvcFramework.ViewEngine;
 using System.Runtime.CompilerServices;
 using System.Text;
 
@@ -6,25 +7,41 @@ namespace SUS.MvcFramework
 {
     public abstract class Controller
     {
-        public HttpResponse View([CallerMemberName]string viewPath = null)
+        private SusViewEngine viewEngine;
+
+        public Controller()
+        {
+            this.viewEngine = new SusViewEngine();
+        }
+
+        public HttpResponse View(
+            object viewModel = null,
+            [CallerMemberName] string viewPath = null)
         {
             var layout = System.IO.File.ReadAllText("Views/Shared/_Layout.cshtml");
-            var responseContent = System.IO.File.ReadAllText("Views/"+ 
-                this.GetType().Name.Replace("Controller",string.Empty) 
-                + "/" + viewPath + ".cshtml");
+            layout = layout.Replace("@RenderBody()", "____VIEW_GOES_HERE____");
+            layout = this.viewEngine.GetHtml(layout, viewModel);
 
-            var responseHtml = layout.Replace("@RenderBody()", responseContent);
+            var viewContent = System.IO.File.ReadAllText(
+                "Views/" +
+                this.GetType().Name.Replace("Controller", string.Empty) +
+                "/" + viewPath + ".cshtml");
+            viewContent = this.viewEngine.GetHtml(viewContent, viewModel);
+
+            var responseHtml = layout.Replace("____VIEW_GOES_HERE____", viewContent);
 
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
             var response = new HttpResponse("text/html", responseBodyBytes);
             return response;
         }
+
         public HttpResponse File(string filePath, string contentType)
         {
             var fileBytes = System.IO.File.ReadAllBytes(filePath);
             var response = new HttpResponse(contentType, fileBytes);
             return response;
         }
+
         public HttpResponse Redirect(string url)
         {
             var response = new HttpResponse(HttpStatusCode.Found);
